9c7a65df334d41e8559abd9323ab1b444edafdc3
zhoyi75615 <86950899@qq.com>
2024-10-12 19:54:50 +0800

    no1

---
+++ ui.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :ui.py.py
+# @Time      :2024/10/12 17:52
+# @Author    :zhou_yi
+import streamlit as st
+
+st.header('ceshi')
+if __name__ == '__main__':
+    ...
